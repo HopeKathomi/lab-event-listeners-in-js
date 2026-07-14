fe4b9cb080cb205f8681cf2fb83bbc5c11b88b2d
Hope Kathomi <kathomihope@gmail.com>
2026-07-14 23:17:34 +0300

    added event listeners to all buttons and the input box

--- index.js
+++ index.js
@@ -3,19 +3,28 @@
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
+    event.stopPropagation();
+    document.body.style.backgroundColor = "blue";
+  
 }
-
+// changeBackgroundColor()
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
   // Implement the function to reset background color
+    document.querySelector("body").style.background = "white";
 }
+// resetBackgroundColor()
 
 // Capture Keyboard Input
 
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+  
+    document.querySelector("#textInputDisplay").textContent = event.key;
+
 }
+// document.addEventListener("keydown", displayKeyPress);
 
 // Process Text Input
 
