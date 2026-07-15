d62fb83fc1d7b061dc0adeb76f654060a6132c09
Hope Kathomi <kathomihope@gmail.com>
2026-07-15 09:38:52 +0300

    changed the queryselector target of displayKeyPress()

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function resetBackgroundColor() {
 function displayKeyPress(event) {
   // Implement the function to display key pressed
   
-    document.querySelector("#textInputDisplay").textContent = event.key;
+    document.querySelector("#keyPressDisplay").textContent = event.key;
 
 
 }
